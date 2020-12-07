50b76d1b7825f5ff109e5543ade9d31d98333c5d
jaehyunii <wogus981125@gmail.com>
2020-12-07 17:28:45 +0900

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+    const { getByText } = render(<App />);
+});
+
+test('renders learn react link', () => {
+    const { getByText } = render(<App />);
+});
+test('renders learn react link', () => {
+    const { getByText } = render(<App />);
+});
